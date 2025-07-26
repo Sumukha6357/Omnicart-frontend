8be88a47981c875170a20543a03c8ed396750aac
Sumukha K Y <107631405+Sumukha6357@users.noreply.github.com>
2025-07-26 14:52:18 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 // 🚨 Use your real backend URL (hosted on Render or wherever)
-const BASE_URL = "https://omnicart-backend.onrender.com"; // change this to your backend URL
+const BASE_URL = " https://omnicart-backend-7ujt.onrender.com"; // change this to your backend URL
 
 const api = axios.create({
   baseURL: BASE_URL,
